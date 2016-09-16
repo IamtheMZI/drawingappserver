ad366bb9357ba91fcf4b23806c80087bdb6a5e5e
Muhammad Zian Ilahee <Muhammad Zian Ilahee>
2016-09-16 04:50:38 -0700

    Drawing App server Second Commit

--- index.js
+++ index.js
@@ -1,23 +1,24 @@
 var server  = require('http').createServer();
 var io      = require('socket.io')(server);
+var tweet={x_val:0,y_val:0,touch:''};
 io.on('connection', function (socket) {  
-    var tweet = {user: "nodesource", txt: "Hello, world!"};
+    
 		console.log("Connected "+ io.sockets.clients());
     // to make things interesting, have it send every second
     var interval = setInterval(function () {
         socket.emit('tweet', tweet);
-		//console.log(tweet);
+		console.log(tweet);
     }, 7000);
 	
 	socket.on('tweet', function (tweet) {
         // we received a tweet from the browser
-
-        console.log(tweet);
+		var sweet = {x_val:tweet.x_val,y_val:tweet.y_val,touch:tweet.touch};
+		socket.emit('tweet',sweet);
+        console.log(sweet);
     });
 
     socket.on('disconnect', function () {
 		console.log("Disconnected "+ io.sockets.clients());
-        clearInterval(interval);
     });
 });
 
