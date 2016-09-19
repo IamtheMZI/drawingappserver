61ed6362b1d0555f713fcfba5ddb08c4e4b1b0ab
Muhammad Zian Ilahee <Muhammad Zian Ilahee>
2016-09-18 23:58:59 -0700

    Working sample

--- index.js
+++ index.js
@@ -1,20 +1,14 @@
 var server  = require('http').createServer();
 var io      = require('socket.io')(server);
-var tweet={x_val:0,y_val:0,touch:''};
 io.on('connection', function (socket) {  
     
 		console.log("Connected "+ io.sockets.clients());
     // to make things interesting, have it send every second
-    var interval = setInterval(function () {
-        socket.emit('tweet', tweet);
-		console.log(tweet);
-    }, 7000);
 	
 	socket.on('tweet', function (tweet) {
         // we received a tweet from the browser
-		var sweet = {x_val:tweet.x_val,y_val:tweet.y_val,touch:tweet.touch};
-		socket.emit('tweet',sweet);
-        console.log(sweet);
+		socket.broadcast.emit('tweet',tweet);
+        console.log(tweet);
     });
 
     socket.on('disconnect', function () {
